286f01cd884763b8b7c62ab35427b703a08683fb
Karina Garcia <karinag.000@gmail.com>
2025-04-09 08:10:05 -0700

    created github workflows

---
+++ server/src/seeds/cleanDb.ts
@@ -0,0 +1,21 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+
+export default async (modelName: "Question", collectionName: string) => {
+  try {
+    const model = models[modelName];
+    if (!model || !model.db || !model.db.db) {
+      throw new Error(`Model or database not found for modelName: ${modelName}`);
+    }
+
+    let modelExists = await model.db.db.listCollections({
+      name: collectionName
+    }).toArray();
+
+    if (modelExists.length) {
+      await db.dropCollection(collectionName);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
